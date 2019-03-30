527fdcd845070d1aad3285f5548dda1626e3f0f6
Suman Saha <sumandroid12@gmail.com>
2019-03-31 00:44:54 +0530

    +20. Valid Parentheses

---
+++ 20.valid-parentheses.cpp
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (36.12%)
+ * Total Accepted:    545.2K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        unordered_map<char,char> closer({{')','('}, {']','['}, {'}','{'}});
+
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('|| s[i]=='{' ||s[i]=='[')
+            st.emplace(s[i]);
+            else if(!st.empty() && st.top()==closer[s[i]])
+            st.pop();
+            else
+            {
+                return false;
+            } 
+        }
+        return true; 
+    }
+};
+
